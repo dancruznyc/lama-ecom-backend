f32679ae52bee92df8ac2f7ec968876563fbaa17
dancruznyc <dancruznyc@gmail.com>
2022-05-16 14:25:18 -0400

    jwts added

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ mongoose
   });
 
 app.use(express.json());
-app.use("/api/auth", userRoute);
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
